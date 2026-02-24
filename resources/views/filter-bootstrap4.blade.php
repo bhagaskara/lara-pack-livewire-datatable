70b01039e615d11578b5d4c6fe23b0265dc93523
Bhagaskara <bhagaskaraliancer@gmail.com>
2026-02-24 13:10:50 +0700

    fix broken border between icon and input in filter

--- resources/views/filter-bootstrap4.blade.php
+++ resources/views/filter-bootstrap4.blade.php
@@ -9,14 +9,14 @@
             $filter['type'] == 'tel')
         <div class="input-group input-group-sm">
             <div class="input-group-prepend">
-                <span class="input-group-text bg-transparent border-right-0">
+                <span class="input-group-text">
                     <svg width="14" height="14" fill="currentColor" viewBox="0 0 16 16" class="text-muted">
                         <path
                             d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001l3.85 3.85a1 1 0 0 0 1.415-1.415l-3.85-3.85Zm-5.242.656a5 5 0 1 1 0-10.001 5 5 0 0 1 0 10Z" />
                     </svg>
                 </span>
             </div>
-            <input type="{{ $filter['type'] }}" class="form-control form-control-sm border-left-0"
+            <input type="{{ $filter['type'] }}" class="form-control form-control-sm"
                 placeholder="{{ $filter['placeholder'] }}"
                 wire:model.live.debounce.500="filterColumn.{{ $index }}.value" />
         </div>
